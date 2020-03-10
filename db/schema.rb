5337e554de30a35136179f30db446d6f0f2ad281
misterMK24 <41155221+misterMK24@users.noreply.github.com>
2020-03-10 17:26:10 +0100

    database.yml configuring
    changing file database.yml for a DRY

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2020_03_03_212854) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "projects", force: :cascade do |t|
     t.string "title"
     t.string "created_by"
@@ -23,7 +26,7 @@ ActiveRecord::Schema.define(version: 2020_03_03_212854) do
     t.string "name_task"
     t.boolean "done"
     t.integer "priority"
-    t.integer "project_id", null: false
+    t.bigint "project_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["project_id"], name: "index_tasks_on_project_id"
